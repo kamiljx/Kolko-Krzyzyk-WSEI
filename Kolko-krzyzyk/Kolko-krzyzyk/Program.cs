a187d1b862b6b265e2127865876c95c7d30018cc
kamiljx <rzeszutek98@gmail.com>
2019-06-09 21:18:22 +0200

    Plansza
    Created game board

---
+++ Kolko-krzyzyk/Kolko-krzyzyk/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Kolko_krzyzyk
+{
+    internal class Program
+    {
+        static char[] tablica = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        public static void Main(string[] args)
+        {
+            Plansza();
+        }
+
+        public static void Plansza()
+        {
+            Console.WriteLine("     █     █      ");
+            Console.WriteLine("  {0}  █  {1}  █  {2}", tablica[1], tablica[2], tablica[3]);
+            Console.WriteLine("■■■■■■■■■■■■■■■■");
+            Console.WriteLine("     █     █      ");
+            Console.WriteLine("  {0}  █  {1}  █  {2}", tablica[4], tablica[5], tablica[6]);
+            Console.WriteLine("■■■■■■■■■■■■■■■■");
+            Console.WriteLine("     █     █     ");
+            Console.WriteLine("  {0}  █  {1}  █  {2}", tablica[7], tablica[8], tablica[9]);
+        }
+    }
+}
---
+++ Kolko-krzyzyk/Kolko-krzyzyk/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Kolko_krzyzyk")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Kolko_krzyzyk")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6D8A6045-E8B9-4761-BC18-DC8571B630E7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
